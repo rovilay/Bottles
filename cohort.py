504732e11659e08a08fb87d2ab13f4f573ef477e
rovilay <rovilayjnr01@gmail.com>
2017-11-05 11:05:11 +0100

    updated

--- cohort.py
+++ cohort.py
@@ -22,7 +22,7 @@ def form():
 
 @post('/plot')
 def submit():
-    # grab data from form
+    # grab data from form ***
     Y01 = request.forms.get('Y01')
     Y02 = request.forms.get('Y02')
     Y03 = request.forms.get('Y03')
